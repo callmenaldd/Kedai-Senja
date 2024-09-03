22b4b2c6e8cf5e4da0d6f45836bd4f841c6e2d9f
callmenaldd <ronalredji@gmail.com>
2024-09-03 23:39:25 +0700

    website cofe senja

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toogle class active
+// Toogle class active untuk hamburger menu
 const navbarNav = document.querySelector
 ('.navbar-nav');
 //ketika hamburger menu di klik
@@ -7,11 +7,25 @@ document.querySelector('#hamburger-menu').onclick = () => {
 
 };
 
-//Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toogle class active untuk search form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
 
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+//Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 document.addEventListener('click', function(e){
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
+    }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
     }
 });
